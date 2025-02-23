f9a8a828418101d04fe5b0670707b84d4c1aa804
zdx <zhangwenming0906@163.com>
2025-02-23 15:49:00 +0000

    project init
    project init

---
+++ src/main/java/com/zdx/springboot3log4j/Springboot3log4jApplication.java
@@ -0,0 +1,13 @@
+package com.zdx.springboot3log4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3log4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3log4jApplication.class, args);
+	}
+
+}
