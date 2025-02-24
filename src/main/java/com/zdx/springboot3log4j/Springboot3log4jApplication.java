96b80178ade9c10716c96448a50d99066b087a30
zdx <zhangwenming0906@163.com>
2025-02-24 13:50:31 +0800

    add log4j2 output

--- src/main/java/com/zdx/springboot3log4j/Springboot3log4jApplication.java
+++ src/main/java/com/zdx/springboot3log4j/Springboot3log4jApplication.java
@@ -1,13 +1,36 @@
 package com.zdx.springboot3log4j;
 
+import com.zdx.springboot3log4j.bean.Persion;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@Slf4j
 public class Springboot3log4jApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Springboot3log4jApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3log4jApplication.class, args);
+    }
+
+
+    @GetMapping("/log4j")
+    public String log4j() {
+        Persion persion = new Persion();
+        persion.setId(1);
+        persion.setName("zdx");
+        persion.setBirthday("1989-01-01");
+        persion.setAge(19);
+
+        for (int i = 0; i < 10; i++) {
+            log.info("The persion is {}", persion);
+        }
+
+        return "hello";
+    }
 
 }
---
+++ src/main/java/com/zdx/springboot3log4j/bean/Persion.java
@@ -0,0 +1,17 @@
+package com.zdx.springboot3log4j.bean;
+
+import lombok.Data;
+
+@Data
+public class Persion {
+
+    public long id;
+
+    public String name;
+
+    public String birthday;
+
+    public int age;
+
+
+}
