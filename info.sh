c97b257b754c895b9e033bdff00e2e8d37672a66
qinshower <zhangwenming0906@163.com>
2025-03-01 17:21:01 +0000

    change port 8070

--- info.sh
+++ info.sh
@@ -1,1 +1,2 @@
-nohup java -Dlog4j.configurationFile=log4j2-info.xml -jar target/springboot3log4j-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+nohup java -Dserver.port=8070 -Dlog4j.configurationFile=log4j2-info.xml -jar target/springboot3log4j-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
+
