a44c31b031585e454bb3b06d3dc3c75771ff0680
qinshower <zhangwenming0906@163.com>
2025-03-01 16:33:27 +0000

    lazygit commit file

--- debug.sh
+++ debug.sh
@@ -1,1 +1,1 @@
-nohup java -Dserver.port=9090 -Dlog4j.configurationFile=log4j2-info.xml -jar target/springboot3log4j-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+nohup java -Dserver.port=8090 -Dlog4j.configurationFile=log4j2-debug.xml -jar target/springboot3log4j-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
